3fdcc9c125020b5f5a8b7d3ca29b10a305946f05
raptor7197 <vamsikrishna75836@gmail.com>
2025-07-08 22:50:23 +0530

    nvim configs added

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+require('config.EcoVim')
+
+require('utils.globals')
+require('utils.functions')
+
+require('config.options')
+require('config.lazy')
+require('config.keymappings')
+require('config.autocmds')
+
+require('internal.cursorword')
